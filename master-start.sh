3e41484f8f748c0ffe52fafa578da9e897f19ec5
Vishnu Vardhan <vardhanv@yahoo.com>
2017-12-21 19:02:08 -0800

    bug fixes

--- master-start.sh
+++ master-start.sh
@@ -39,34 +39,30 @@ if [ -z ${HOST_IP_ADDR} ]; then
     echo "HOST_IP_ADDR needs to be set"; 
     echo "run: $0 --configure to configure your environment first"
     exit 1
-else 
-    echo "Using HOST_IP_ADDR: $HOST_IP_ADDR"; 
 fi
 
 if [ -z ${AWS_ACCESS_KEY_ID} ]; then 
     echo "AWS_ACCESS_KEY_ID needs to be set"; 
     echo "run: $0 --configure to configure your environment first"
     exit 1
-else 
-    echo "Using AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"; 
-    echo "run: $0 --configure to configure your environment first"
 fi
 
 if [ -z ${AWS_SECRET_ACCESS_KEY} ]; then 
     echo "AWS_SECRET_ACCESS_KEY needs to be set"; 
     echo "run: $0 --configure to configure your environment first"
     exit 1
-else 
-    echo "Using AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"; 
 fi
 
 if [ -z ${MY_TARGET} ]; then 
     echo "MY_TARGET needs to be set"; 
     echo "run: $0 --configure to configure your environment first"
     exit 1
-else 
-    echo "Using MY_TARGET: $MY_TARGET"; 
 fi
+
+echo "Using AWS_ACCESS_KEY_ID     : $AWS_ACCESS_KEY_ID"; 
+echo "Using AWS_SECRET_ACCESS_KEY : $AWS_SECRET_ACCESS_KEY"; 
+echo "Using MY_TARGET             : $MY_TARGET"; 
+echo "Using HOST_IP_ADDR          : $HOST_IP_ADDR"; 
 
 docker run --shm-size 1G -v /tmp:/tmp -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e HOST_IP_ADDR -p 25521:25521/udp vardhanv/cosbench_ng -e $MY_TARGET "$@"
 
